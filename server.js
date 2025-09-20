9a4b8038f14f5159502b8cdfed5d37bd02187a4f
shawnp221 <snpatel@wpi.edu>
2025-09-20 19:17:58 -0400

    Change a2 to a3 it callback url

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ passport.use(new GitHubStrategy({
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
         // callbackURL: "http://localhost:3000/auth/github/callback"
-        callbackURL: "https://a2-shawnpatel.onrender.com/auth/github/callback"
+        callbackURL: "https://a3-shawnpatel.onrender.com/auth/github/callback"
     },
     async (accessToken, refreshToken, profile, done) => {
         try {
